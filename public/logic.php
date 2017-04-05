b1bfa32f9af15507a841a897988c682dcb82fd96
Malcolm Littlefield <mhlittlefield@gmail.com>
2017-04-04 22:00:02 -0400

    First major work session

---
+++ public/logic.php
@@ -0,0 +1,49 @@
+<?php
+require('Form.php');
+# require('tools.php');
+use DWA\Form;
+
+# sanitize any GET Data
+# $_GET = sanitize($_GET);
+
+# instantiate a new form
+$form = new Form($_GET);
+
+# bring form values into useful variables
+$tab = $form->get('tab', '');
+$partySize = $form->get('partySize', '');
+$quality = $form->get('quality', '');
+$roundUp = $form->isChosen('roundUp');
+$errors = false;
+
+if ($form->isSubmitted()) {
+
+  $errors = $form->validate([
+    'tab' => 'required|min:0',
+    'partySize' => 'required|min:1',
+  ]);
+
+  # quantify the tab
+  $tab = floatval($tab);
+
+  #quantify the party size
+  $partySize = intval($partySize);
+
+  # quantify the quality
+  $tipFactor = 1.18;
+  if ($quality == 'bad') {
+    $tipFactor = 1.14;
+  } elseif ($quality == 'excellent') {
+    $tipFactor = 1.22;
+  }
+
+  # calculate the amount each person owes.
+  $amount = $tab*$tipFactor/$partySize;
+
+  # round up if necessary
+  if ($roundUp == true) {
+    $amount = ceil($amount);
+  } else {
+    $amount = round($amount, 2);
+  }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,54 @@
+<?php require('logic.php'); ?>
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title>Bill Splitter</title>
+    <meta charset='utf-8'>
+    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
+    <link href='https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/paper/bootstrap.min.css' rel='stylesheet'>
+    <link href='css/a2.css' rel='stylesheet'>
+</head>
+<body>
+    <h1>Bill Splitter</h1>
+
+    <form method='get' action='index.php' class='form-horizontal'>
+      <div><!--get the tab-->
+        <label for='tab'>How much is the tab? $</label>
+        <input type='number' name='tab' id='tab' min='0' step="0.01" value='{{$tab}}' requried>
+        <div class='text-warning'>Required</div>
+      </div>
+      <div><!--get the party size-->
+        <label for='partySize'>Split amongst how many? </label>
+        <input type='number' name='partySize' id='partySize' min='1' value='{{$partySize}}' required>
+        <div class='text-warning'>Required</div>
+      </div>
+      <div><!--get the quality of service-->
+      <label for='quality'>How was the service?</label>
+      <select name='quality' id='quality'>
+          <option value='bad' @if ($quality=='bad') selected @endif>Bad (15% tip)</option>
+          <option value='good' @if ($quality!='excellent' and $quality!='bad') selected @endif>Good (18% tip)</option>
+          <option value='excellent' @if ($quality=='excellent') selected @endif>Excellent (22% tip)</option>
+      </select>
+    </div>
+    <div><!--round up?-->
+      <label for='roundUp'>Round up?</label>
+      <input type='checkbox' name='roundUp' id='roundUp' @if ($roundUp == true) CHECKED @endif>
+    </div><!--submit-->
+      <input type='submit' class='btn btn-primary' value='submit'>
+    </form>
+
+    @if ($errors)
+    <div class='alert alert-danger'>
+      <ul>
+      @foreach ($errors as $error)
+        <li>{{$error}}</li>
+      @endforeach
+      </ul>
+    </div>
+    @elseif ($form->isSubmitted())
+    <div class='alert alert-success'>
+      Each person owes <strong>${{$amount}}.</strong>
+    </div>
+    @endif
+</body>
+</html>
